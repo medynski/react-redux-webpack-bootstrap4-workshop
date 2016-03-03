9399a7e485c1656dfdedf45a78b94808ba879fc6
wmedynski <ggmedi@o2.pl>
2016-03-03 09:29:24 +0100

    changed prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,16 @@
-var webpack           = require("webpack");
-var path              = require("path");
-var HTMLWebpackPlugin = require("html-webpack-plugin");
+var webpack = require('webpack');
+var path = require('path');
+var zlib = require('zlib');
+
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var WebpackMd5Hash    = require('webpack-md5-hash');
+
+var OccurenceOrderPlugin = require('webpack/lib/optimize/OccurenceOrderPlugin');
+var DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+var CompressionPlugin = require('compression-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var config = {
   devtool: "inline-source-map",
@@ -23,10 +33,52 @@ var config = {
   },
 
   plugins: [
+    new WebpackMd5Hash(),
+    new DedupePlugin(),
+    new OccurenceOrderPlugin(true),
+    new CommonsChunkPlugin({
+      name: 'polyfills',
+      filename: 'polyfills.[chunkhash].bundle.js',
+      chunks: Infinity
+    }),
+    // static assets
+    new CopyWebpackPlugin([
+      {
+        from: 'src/assets',
+        to: 'assets'
+      }
+    ]),
+    // generating html
     new HTMLWebpackPlugin({
       template: path.resolve("src/index.html"),
       minify: { collapseWhitespace: false },
     }),
+    new UglifyJsPlugin({
+      // to debug prod builds uncomment //debug lines and comment //prod lines
+
+      // beautify: true,//debug
+      // mangle: false,//debug
+      // dead_code: false,//debug
+      // unused: false,//debug
+      // deadCode: false,//debug
+      // compress : { screw_ie8 : true, keep_fnames: true, drop_debugger: false, dead_code: false, unused: false, }, // debug
+      // comments: true,//debug
+
+      beautify: false,//prod
+      // disable mangling because of a bug in angular2 beta.1, beta.2 and beta.3
+      // TODO(mastertinner): enable mangling as soon as angular2 beta.4 is out
+      // mangle: { screw_ie8 : true },//prod
+      mangle: false,
+      compress : { screw_ie8 : true},//prod
+      comments: false//prod
+
+    }),
+    // include uglify in production
+    new CompressionPlugin({
+      algorithm: gzipMaxLevel,
+      regExp: /\.css$|\.html$|\.js$|\.map$/,
+      threshold: 2 * 1024
+    })
   ],
 
   module: {
@@ -41,3 +93,7 @@ var config = {
 };
 
 module.exports = config;
+
+function gzipMaxLevel(buffer, callback) {
+  return zlib['gzip'](buffer, {level: 9}, callback)
+}
